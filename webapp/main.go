1a33e567b133507fd7df42c712882c395fca4880
hogedigo <ikeda@knightso.co.jp>
2014-04-19 09:13:46 +0900

    test hello world

--- webapp/main.go
+++ webapp/main.go
@@ -7,14 +7,13 @@ import (
 	_ "github.com/hogedigo/shizgo/webapp/handler"
 )
 
-func helloWorld(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm() // formパラメータを解析する
-	fmt.Fprintf(w, "Hello " + r.FormValue("name") + "!")
+func helloWorld(w http.ResponseWriter, _ *http.Request) {
+	fmt.Fprintf(w, "Hello world!")
 }
 
 func main() {
 	http.HandleFunc("/hello", helloWorld)       //アクセスのルーティングを設定します。
-	err := http.ListenAndServe(":9876", nil) //監視するポートを設定します。
+	err := http.ListenAndServe(":9876", nil)
 	if err != nil {
 		log.Fatal("error occurred: ", err)
 	}
